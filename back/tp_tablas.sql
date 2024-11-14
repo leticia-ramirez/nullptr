4073e272527a9f4779ab760bd1de6243caac9673
Zumbyzoonvy <flcoronel@fi.uba.ar>
2024-11-14 16:30:25 -0300

    KAN-9 Correccion creacion de tablas

--- back/tp_tablas.sql
+++ back/tp_tablas.sql
@@ -1,32 +1,38 @@
 USE TP_IDS;
 
--- TABLA DE REPORTES - Se comunica con la tabla INCIDETES y la tabla USUARIOS
-CREATE TABLE reportes (
-	ID_reporte INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ciudad VARCHAR(100) NOT NULL,
-    localidad VARCHAR(100) NOT NULL,
-    fecha_reporte DATE NOT NULL,
-    horario_reporte TIME NOT NULL,
-    FOREIGN KEY (ID_incidente) REFERENCES incidentes(ID_incidentes),
-    FOREIGN KEY (ID_usario) REFERENCES usuarios(ID_usuario)
-);
-
 -- TABLA DE INCIDENTES - Se comunica con la tabla REPORTES
 CREATE TABLE incidentes (
-	ID_incidente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	ID_incidente INT NOT NULL AUTO_INCREMENT,
     tipo_reporte VARCHAR(50) NOT NULL,
     direccion_reporte VARCHAR(100) NOT NULL,
-    descripcion VARCHAR(100) NOT NULL
+    descripcion VARCHAR(100) NOT NULL,
+	PRIMARY KEY (ID_incidente)
 );
 
 -- TABLA DE USUARIOS - Se comunica con la tabla REPORTES
 CREATE TABLE usuarios (
-	ID_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	ID_usuario INT NOT NULL AUTO_INCREMENT,
     nombre_usuario VARCHAR(20) NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    telefono VARCHAR(50) NOT NULL
+    telefono VARCHAR(50) NOT NULL,
+    PRIMARY KEY (ID_usuario)
 );
+
+-- TABLA DE REPORTES - Se comunica con la tabla INCIDETES y la tabla USUARIOS
+CREATE TABLE reportes (
+	ID_reporte INT NOT NULL AUTO_INCREMENT,
+    ciudad VARCHAR(100) NOT NULL,
+    localidad VARCHAR(100) NOT NULL,
+    fecha_reporte DATE NOT NULL,
+    horario_reporte TIME NOT NULL,
+    ID_incidente INT,
+    ID_usuario INT,
+    PRIMARY KEY (ID_reporte),
+    FOREIGN KEY (ID_incidente) REFERENCES incidentes(ID_incidente),
+    FOREIGN KEY (ID_usario) REFERENCES usuarios(ID_usuario)
+);
+
 
 -- QUEDA PARA HACER EL HARDCODEO DE DATOS
