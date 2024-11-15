b5e36cd4d0e8bc9479dcd6bc8079b49a448f1410
leticia-ramirez <llramirezv@fi.uba.ar>
2024-11-15 11:23:17 -0300

    arreglo codigo

--- front/static/js/script_mapa.js
+++ front/static/js/script_mapa.js
@@ -1,16 +1,18 @@
 function iniciar_mapa() {
-	var coordenadas_centrales = {lat: -34.595657, lng: -58.451228};
-	var mapa = new google.maps.Map(document.getElementById('map'), {zoom: 10, center: coordenadas_centrales});
+	let coordenadas_centrales = {lat: -34.595657, lng: -58.451228};
+	let mapa = new google.maps.Map(document.getElementById('map'), {zoom: 10, center: coordenadas_centrales});
 
 	// COORDENADAS HARDCODEADAS PARA PROBAR FUNCIONALIDAD
 
-	var coordenada_1_hardcodeada = {lat: -34.617745,lng: -58.367399};
-	var marcador_1 = new google.maps.Marker({position: coordenada_1_hardcodeada, map: mapa});
-	var coordenada_2_hardcodeada = {lat: -34.588711,lng: -58.429723};
-	var marcador_2 = new google.maps.Marker({position: coordenada_2_hardcodeada, map: mapa});
-	var coordenada_3_hardcodeada = {lat: -34.590165,lng: -58.372720};
-	var marcador_3 = new google.maps.Marker({position: coordenada_3_hardcodeada, map: mapa});
+	let coordenada_1_hardcodeada = {lat: -34.617745,lng: -58.367399};
+	let marcador_1 = new google.maps.Marker({position: coordenada_1_hardcodeada, map: mapa});
+	let coordenada_2_hardcodeada = {lat: -34.588711,lng: -58.429723};
+	let marcador_2 = new google.maps.Marker({position: coordenada_2_hardcodeada, map: mapa});
+	let coordenada_3_hardcodeada = {lat: -34.590165,lng: -58.372720};
+	let marcador_3 = new google.maps.Marker({position: coordenada_3_hardcodeada, map: mapa});
 }
+
+iniciar_mapa()
 
 // function iniciar_mapa() {
 //     var coordenadas_centrales = {lat: -34.595657, lng: -58.451228};
