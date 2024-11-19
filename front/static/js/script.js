203cd872646f15b26686856e7dd5e136cf492074
leticia-ramirez <llramirezv@fi.uba.ar>
2024-11-19 05:52:54 -0300

    agrego api de buenos aires que se ven reflejados en los selects

---
+++ front/static/js/script.js
@@ -0,0 +1,47 @@
+// select relacionados
+
+const selectProvincias=document.getElementById('provincia');
+const selectMunicipios= document.getElementById('municipio');
+const selectLocalidades=document.getElementById('localidad');
+function provincia(){
+    fetch("https://apis.datos.gob.ar/georef/api/provincias")
+    .then(res=> res.ok ? res.json() : Promise.reject(res))
+    .then(json=>{
+        let options=`<option value="Elige una provincia">Elige una provincia</option>`;
+        json.provincias.forEach(el => options +=`<option value="${el.nombre}">${el.nombre}</option>`);
+        selectProvincias.innerHTML= options;
+
+    })
+    
+}
+document.addEventListener("DOMContentLoaded", provincia)
+
+function municipio(provincia){
+    fetch(`https://apis.datos.gob.ar/georef/api/municipios?provincia=${provincia}`)
+    .then(res=> res.ok ? res.json() : Promise.reject(res))
+    .then(json=>{
+        let options=`<option value="Elige un municipio">Elige un municipio</option>`;
+        json.municipios.forEach(el => options +=`<option value="${el.id}">${el.nombre}</option>`);
+        selectMunicipios.innerHTML= options;
+    })
+}
+selectProvincias.addEventListener("change", el=>{
+    municipio(el.target.value);
+    console.log(el.target.value);
+})
+
+function localidad(municipio){
+    fetch(`https://apis.datos.gob.ar/georef/api/localidades?municipio=${municipio}`)
+    .then(res=> res.ok ? res.json() : Promise.reject(res))
+    .then(json=>{
+        let options=`<option value="Elige una localidad">Elige una localidad</option>`;
+        
+        json.localidades.forEach(el => options +=`<option value="${el.id}">${el.nombre}</option>`);
+        selectLocalidades.innerHTML= options;
+    })
+}
+selectMunicipios.addEventListener("change", el=>{
+    localidad(el.target.value);
+    console.log(el.target.value);
+    console.log(el);
+})
