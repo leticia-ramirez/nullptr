7b07e34f73523e76f1853d34407409e8f26b7547
Brenda Ibañez <bibanez@fi.uba.ar>
2024-11-21 16:13:11 -0300

    KAN-17 Correccion en api

--- front/init.sh
+++ front/init.sh
@@ -3,8 +3,10 @@ python3 -m venv .venv
 . .venv/bin/activate
 
 pip install Flask
+pip install requests
 
 
 #Dependencias necesarias para conectar a mysql desde flask.
 pip install flask_sqlalchemy
-pip install mysqlclient
+pip install mysql-connector-python
+#pip install mysqlclient
