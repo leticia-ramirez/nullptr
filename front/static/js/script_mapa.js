da35929d313820a111a441db94eccd8ce868ebb4
Zumbyzoonvy <flcoronel@fi.uba.ar>
2024-11-26 01:24:28 -0300

    KAN-5 // KAN-6 // KAN-11 Actualizacion del front

--- front/static/js/script_mapa.js
+++ front/static/js/script_mapa.js
@@ -1,32 +1,75 @@
+let mapa, ubicacion_actual;
+
 function iniciar_mapa() {
-	let coordenadas_centrales = {lat: -34.595657, lng: -58.451228};
-	let mapa = new google.maps.Map(document.getElementById('map'), {zoom: 10, center: coordenadas_centrales});
-	
-	// console.log(latitud);
-	// console.log(longitud);
-	// let latitud_1 = parseFloat(latitud);
-	// let longitud_1 = parseFloat(longitud);
-	// console.log(latitud_1);
-	// console.log(longitud_1);
+	let coordenadas_centrales = { lat: -34.595657, lng: -58.451228 };
+    mapa = new google.maps.Map(document.getElementById("map"), {zoom: 15, center: coordenadas_centrales});
+     
+    let icono_customizado = {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        fillColor: "#08cf3d",
+        fillOpacity: 0.8,
+        strokeWeight: 2,
+        strokeColor: "#12421f",
+    };
 
-	// let coordenada = { lat: latitud_1, lng: longitud_1};
-	// let advancedMarker = new google.maps.Marker({position: coordenada, map: mapa});
+    let icono_incidencias = {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        fillColor: "#e00934",
+        fillOpacity: 0.8,
+        strokeWeight: 2,
+        strokeColor: "#851027",
+    };
 
-	// console.log("Coordenadas del mapa:", coordenadas);
+    ubicacion_actual = new google.maps.Marker({
+        position: coordenadas_centrales,
+        map: mapa,
+        icon: icono_customizado,
+        title: "Tu ubicación actual",
+    });
 
-	coordenadas.forEach((coordenada, index) => {
-        // console.log(`Creando marcador ${index + 1} en:`, coordenada);
-        
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+        actualizar_posicion,
+        recibir_error,
+        { enableHighAccuracy: true, maximumAge: 0 }
+        );
+    } else {
+        alert("La geolocalización no está soportada por tu navegador.");
+    }
+
+    coordenadas.forEach((coordenada, index) => {
         if (coordenada.lat && coordenada.lng) {
             new google.maps.Marker({
                 position: { lat: coordenada.lat, lng: coordenada.lng },
                 map: mapa,
+                icon: icono_incidencias,
             });
-        } else {
-            // console.warn(`Coordenada inválida en el índice ${index}:`, coordenada);
         }
     });
+}
 
-	let coordenada_hardcodeada = {lat: -34.590165, lng: -58.372720};
-	let marcador_3 = new google.maps.Marker({position: coordenada_hardcodeada, map: mapa});
-}	
+function actualizar_posicion(position) {
+    let { latitude, longitude } = position.coords;
+    let nueva_posicion = { lat: latitude, lng: longitude };
+
+    ubicacion_actual.setPosition(nueva_posicion);
+    mapa.setCenter(nueva_posicion);
+}
+
+function recibir_error(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("Permiso denegado para acceder a la ubicación.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Información de ubicación no disponible.");
+            break;
+        case error.TIMEOUT:
+            alert("Tiempo de espera agotado al intentar obtener la ubicación.");
+            break;
+        default:
+            alert("Error desconocido al intentar acceder a la ubicación.");
+    }
+}
